55389e53b57fdfc2dfb380a055b509492316053b
Irene Matamala Muñoz <136511839+IreneMatamala@users.noreply.github.com>
2025-12-21 19:32:05 +0100

    Initialize Flask app with environment configuration

--- src/microservice-a/wsgi.py
+++ src/microservice-a/wsgi.py
@@ -1,1 +1,9 @@
+from app import create_app
+import os
 
+# Determinar entorno
+env = os.getenv('FLASK_ENV', 'default')
+app = create_app(env)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000)
