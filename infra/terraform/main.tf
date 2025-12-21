0c656cc039bcf3d8c62f965ee51cf2a5a2661825
Irene Matamala Muñoz <136511839+IreneMatamala@users.noreply.github.com>
2025-12-21 12:54:03 +0100

    Update main.tf

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,1 +1,19 @@
 
+provider "libvirt" {
+  uri = "qemu:///system"
+}
+resource "libvirt_volume" "os_image" {
+  name = "ubuntu-22.04.qcow2"
+  pool = "default"
+  source = "https://cloud-images.ubuntu.com/releases/22.04/release/ubuntu-22.04-server-cloudimg-amd64.img"
+}
+resource "libvirt_domain" "k8s_master" {
+  name   = "k8s-master-01"
+  memory = "8192"
+  vcpu   = 4
+  disk { volume_id = libvirt_volume.os_image.id }
+  network_interface {
+    network_name = "default"
+    addresses    = ["192.168.122.10"]
+  }
+}
