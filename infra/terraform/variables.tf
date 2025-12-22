1e8ceac2568b832275ead85a1b4f66943b5c2ea9
Irene Matamala Muñoz <136511839+IreneMatamala@users.noreply.github.com>
2025-12-22 10:28:53 +0100

    Update variables.tf

--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -1,1 +1,27 @@
+variable "vsphere_user" {}
+variable "vsphere_password" {
+  sensitive = true
+}
+variable "vsphere_server" {}
+
+variable "datacenter" {}
+variable "cluster" {}
+variable "datastore" {}
+variable "network_name" {}
+
+variable "vm_template" {
+  description = "Template Linux hardened"
+}
+
+variable "vm_count" {
+  default = 2
+}
+
+variable "vm_cpu" {
+  default = 4
+}
+
+variable "vm_memory" {
+  default = 8192
+}
 
