3691a7340a0ff31223769669bc41089483fce72b
Irene Matamala Muñoz <136511839+IreneMatamala@users.noreply.github.com>
2025-12-22 11:37:58 +0100

    Delete tools directory

--- tools/backup-script.sh
+++
@@ -1,7 +0,0 @@
-
-#!/bin/bash
-BACKUP_DIR="/backup/postgres"
-DATE=$(date +%Y%m%d_%H%M%S)
-pg_dump -h postgres-primary -U postgres mydb | gzip > $BACKUP_DIR/mydb_$DATE.sql.gz
-# Rotación: mantener últimos 7 días
-find $BACKUP_DIR -type f -mtime +7 -delete
